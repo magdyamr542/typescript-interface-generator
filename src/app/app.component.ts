b97fdd36c145d2e5f49f1299c842ce93fc5d61b8
amr.metwally <amr.metwally@logarithmo.de>
2020-12-01 03:44:54 +0100

    added options buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { jsonSamples } from 'src/json';
 import { InterfaceGeneratorService } from './interface-generator.service';
 import { syntaxHighlight } from './utils';
 
@@ -14,6 +15,7 @@ export class AppComponent {
   title = 'angular';
   json = 'empty...';
   showErrorMsg = false;
+  showCopyMsg = false;
   interface = 'empty...';
   @ViewChild('jsonInput') jsonInput;
 
@@ -42,4 +44,26 @@ export class AppComponent {
       this.showErrorMsg = false;
     }, 3000);
   }
+
+
+
+  /* Showing the error msg for 3 Seconds */
+  _showCopyMsg() {
+    this.showCopyMsg = true;
+    setTimeout(() => {
+      this.showCopyMsg = false;
+    }, 3000);
+  }
+
+
+  _copyJsonToClipboard(index:number){
+    const json = jsonSamples[index];
+  navigator.clipboard.writeText(json).then(() => {
+    console.log('Async: Copying to clipboard was successful!');
+    this._showCopyMsg();
+  }).catch(() => {
+    console.log('Async: Could not copy to the clipboard!')
+  })
+}
+  
 }
--- src/json.ts
+++ src/json.ts
@@ -1,29 +1,113 @@
 export const json = {
-  name: 'bob',
-  age: 12,
-  married: false,
-  working: true,
-  carType: null,
-  hobbies: ['running', 'grilling'],
-  available: null,
-  favouriteLaptop: {
-    lapName: 'mac',
-    lapPreis: 1200,
-    lapColor: 'grey',
-    lapData: {
-      cpu: '1.2Ghz',
-      storage: 255,
+  "name": 'bob',
+  "age": 12,
+  "married": false,
+  "working": true,
+  "carType": null,
+  "hobbies": ['running', 'grilling'],
+  "available": null,
+  "favouriteLaptop": {
+    "lapName": 'mac',
+    "lapPreis": 1200,
+    "lapColor": 'grey',
+    "lapData": {
+      "cpu": '1.2Ghz',
+      "storage": 255,
     },
   },
-  gridData: [
+  "gridData": [
     [
       { x: 1, y: 2 },
       { x: 2, y: 3 },
     ],
   ],
-  gridDataWithOptionalParams: [
+  "gridDataWithOptionalParams": [
     { x: 1, y: 2 },
     { x: 1, y: 2, z: 3 },
     { x: 1, y: 2, z: 3, f: 12 },
   ],
 };
+
+
+const firstJson = `{
+  "quiz": {
+      "sport": {
+          "q1": {
+              "question": "Which one is correct team name in NBA?",
+              "options": [
+                  "New York Bulls",
+                  "Los Angeles Kings",
+                  "Golden State Warriros",
+                  "Huston Rocket"
+              ],
+              "answer": "Huston Rocket"
+          }
+      },
+      "maths": {
+          "q1": {
+              "question": "5 + 7 = ?",
+              "options": [
+                  "10",
+                  "11",
+                  "12",
+                  "13"
+              ],
+              "answer": "12"
+          },
+          "q2": {
+              "question": "12 - 8 = ?",
+              "options": [
+                  "1",
+                  "2",
+                  "3",
+                  "4"
+              ],
+              "answer": "4"
+          }
+      }
+  }
+}`;
+
+
+ const secondJson = `{"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}`;
+
+ const thirdJson = `{
+  "name": 'bob',
+  "age": 12,
+  "married": false,
+  "working": true,
+  "carType": null,
+  "hobbies": ['running', 'grilling'],
+  "available": null,
+  "favouriteLaptop": {
+    "lapName": 'mac',
+    "lapPreis": 1200,
+    "lapColor": 'grey',
+    "lapData": {
+      "cpu": '1.2Ghz',
+      "storage": 255,
+    },
+  },
+  "gridData": [
+    [
+      { "x": 1, "y": 2 },
+      { "x": 2, "y": 3 },
+    ],
+  ],
+  "gridDataWithOptionalParams": [
+    { "x": 1, "y": 2 },
+    { "x": 1, "y": 2, "z": 3 },
+    { "x": 1, "y": 2, "z": 3, "f": 12 },
+  ],
+}`;
+export const jsonSamples = [firstJson , secondJson , thirdJson];
